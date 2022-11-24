ce88da04fe65a66943e58aba93a2ec698c742e57
Hercules Machado <herculesprogramador911@gmail.com>
2022-11-24 01:16:05 -0300

    first commit

---
+++ tempoDeJogo_1046/Program.cs
@@ -0,0 +1,39 @@
+﻿namespace tempoDeJogo_1046
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            string[] timming = Console.ReadLine().Split();
+            // Variables of input and output
+            int start = int.Parse(timming[0]);
+            int theEnd = int.Parse(timming[1]);
+
+            int tempoMax = 24;
+            int counter = 0;
+
+            // se o meu start for diferente que o fim, rode a função
+            while (start != theEnd)
+            {
+                // aumenta o contador e o seu start em 1
+                counter++;
+                start++;
+
+                // se o start for igual ao tempoMax então start é 0
+                if (start == tempoMax)
+                {
+                    start = 0;
+                }
+
+                // Bug... quando executado 0 0 no console, o valor 'tempo' tem que ser de 24h, o resultado saido é 0. CORRIGIR!!
+            }
+
+            Console.WriteLine($"O JOGO DUROU {counter} HORA(S)");
+
+            
+
+            
+
+        }
+    }
+}
