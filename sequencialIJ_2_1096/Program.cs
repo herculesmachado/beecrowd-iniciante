01e605db1bbc219cc46876001d75d35579810142
Hercules Machado <herculesprogramador911@gmail.com>
2022-11-30 22:21:17 -0300

    Resolvido

---
+++ sequencialIJ_2_1096/Program.cs
@@ -0,0 +1,27 @@
+﻿namespace sequencialIJ_2_1096
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            int i, j;
+            i = 1;
+            j = 7;
+
+            while (i <= 9)
+            {
+                while (j >= 5)
+                {
+                    Console.WriteLine($"I={i} J={j}");
+                    // Pega j e atribui o valor de j - 1
+                    j = j -1;
+                }
+                
+                // Pega i e atribui o valor de i + 1
+                i = i + 2;
+                // Executa ate j ser 7
+                j = 7;
+            }
+        }
+    }
+}
