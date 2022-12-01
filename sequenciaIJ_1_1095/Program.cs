76ec0c98467c393d99d1d4ff790d2dbf0b59520f
Hercules Machado <herculesprogramador911@gmail.com>
2022-11-30 22:18:51 -0300

    Resolvido

---
+++ sequenciaIJ_1_1095/Program.cs
@@ -0,0 +1,21 @@
+﻿namespace sequenciaIJ_1_1095
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            int num1, num2;
+
+            num1 = 1;
+            num2 = 60;
+
+            // atribuindo os valores iniciais num1 e num2
+            // se num2 for menor igual a 60
+            // então: some 3 ao num1 e diminua 5 do num2
+            for (num1=1, num2=60; num2 <= 60; num1+=3, num2 -= 5)
+            {
+                Console.WriteLine($"I={num1} J={num2}");
+            }
+        }
+    }
+}
