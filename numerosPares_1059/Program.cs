554e4e6918427c28ebd04fb0daf86231cb1397d3
Hercules Machado <herculesprogramador911@gmail.com>
2022-11-30 22:15:29 -0300

    Resolvido

---
+++ numerosPares_1059/Program.cs
@@ -0,0 +1,13 @@
+﻿namespace numerosPares_1059
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            for(int i = 1; i <= 50; i++)
+            {
+                Console.WriteLine(i * 2);
+            }
+        }
+    }
+}
