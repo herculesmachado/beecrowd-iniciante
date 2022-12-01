a9fb4f0f9c33ad49c415b29a0079caab2eb88f1e
Hercules Machado <herculesprogramador911@gmail.com>
2022-11-30 21:36:04 -0300

    Resolvido

---
+++ crescenteDecrescente_1113/Program.cs
@@ -0,0 +1,25 @@
+﻿namespace crescenteDecrescente_1113
+{
+    public class Progrma
+    {
+        public static void Main(string[] args)
+        {
+            int x, y;
+            do
+            {
+                string[] values = Console.ReadLine().Split();
+                y = int.Parse(values[1]);
+                x = int.Parse(values[0]);
+                
+                if (x < y)
+                {
+                    Console.WriteLine("Crescente");
+                } else if (x > y)
+                {
+                    Console.WriteLine("Decrescente");
+                }
+
+            } while (x != y);
+        }
+    }
+}
