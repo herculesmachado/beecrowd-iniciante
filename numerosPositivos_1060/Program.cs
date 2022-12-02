d60511c2ccaf602d32fa554eb53df7ffb6d882e4
Hercules Machado <herculesprogramador911@gmail.com>
2022-12-01 23:27:41 -0300

    Resolvido

---
+++ numerosPositivos_1060/Program.cs
@@ -0,0 +1,24 @@
+﻿namespace numerosPositivos_1060
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            double num;
+            int value;
+
+            value = 0;
+
+            for (int i = 1; i <= 6; i++)
+            {
+                num = double.Parse(Console.ReadLine());
+                if (num >= 0)
+                {
+                    value += 1;
+                }
+            }
+
+            Console.WriteLine($"{value}valores positivos");
+        }
+    }
+}
