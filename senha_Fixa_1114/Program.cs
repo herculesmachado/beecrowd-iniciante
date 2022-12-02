e9ad4d799d00d9eb937569b8c51247f1376eae46
Hercules Machado <herculesprogramador911@gmail.com>
2022-12-01 23:25:05 -0300

    Resolvido

---
+++ senha_Fixa_1114/Program.cs
@@ -0,0 +1,21 @@
+﻿namespace senha_Fixa_1114
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            int senha = 1;
+            while (senha != 2002)
+            {
+                senha = int.Parse(Console.ReadLine());
+                if (senha == 2002)
+                {
+                    Console.WriteLine("Acesso Permitido");
+                } else 
+                {
+                    Console.WriteLine("Senha Invalida");
+                }
+            }
+        }
+    }
+}
