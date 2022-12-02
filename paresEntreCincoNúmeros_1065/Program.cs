19f458c11a02b78c45711551eb447a342f6e3c32
Hercules Machado <herculesprogramador911@gmail.com>
2022-12-01 23:29:42 -0300

    Resolvido

---
+++ paresEntreCincoNúmeros_1065/Program.cs
@@ -0,0 +1,21 @@
+﻿namespace paresEntreCincoNúmeros_1065
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            int num, couter = 0;
+
+            for (int i = 0; i < 5; i++)
+            {
+                num = int.Parse(Console.ReadLine());
+                if (num % 2 == 0)
+                {
+                    couter++;
+                }
+            }
+
+            Console.WriteLine($"{couter} valores pares");
+        }
+    }
+}
