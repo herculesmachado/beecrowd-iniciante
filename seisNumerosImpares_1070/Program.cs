04bb23b611f05dc7a9611bedeb3d1746441d09bb
Hercules Machado <herculesprogramador911@gmail.com>
2022-12-01 23:34:06 -0300

    Resolvido

---
+++ seisNumerosImpares_1070/Program.cs
@@ -0,0 +1,22 @@
+﻿namespace seisNumerosImpares_1070
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            int n, i = 0;
+
+            n = int.Parse(Console.ReadLine());
+
+            while (i < 6)
+            {
+                if (n % 2 != 0)
+                {
+                    Console.WriteLine(n);
+                    i++;
+                }
+                n++;
+            }
+        }
+    }
+}
